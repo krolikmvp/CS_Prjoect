365750777a97fa92f11115d857bae1a91528d15f
Damian <dkrolikowski6@gmail.com>
2015-08-24 11:06:08 +0200

    Client checks for bad input

--- Client/src/client.c
+++ Client/src/client.c
@@ -5,12 +5,19 @@
 #include <netdb.h>
 #include <string.h> 
 #include<stdlib.h>
+#define _cd "cd"
+#define _ls "ls"
+#define _pwd "pwd"
+#define _cat "cat"
+#define _exit "exit"
 
 void error(char *msg)
 {
     perror(msg);
     exit(0);
 }
+
+int validate_send(char *);
 
 int main(int argc, char *argv[])
 {
@@ -40,12 +47,20 @@ int main(int argc, char *argv[])
          server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-   
+           error("ERROR connecting");
+
   for(;;){
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
+	int valid=0;
+
+	do{
+		printf("Please enter the message: ");
+    		bzero(buffer,256);
+    		fgets(buffer,255,stdin);
+		valid=validate_send(buffer);		
+	}while(valid==0);
+
+    if(valid<0) break;   
+
     n = write(sockfd,buffer,strlen(buffer)-1);
     if (n < 0) 
          error("ERROR writing to socket");
@@ -55,6 +70,47 @@ int main(int argc, char *argv[])
          error("ERROR reading from socket");
     printf("%s\n",buffer);
 	}
+
+    close(sockfd);
+    printf("Closing client\n");
     return 0;
 
 }
+
+int validate_send(char * buff){
+
+	if(!strncmp(buff,_cd,strlen(_cd))){
+		if( (strlen(buff)-1)==strlen(_cd) ){
+		     printf("ERROR: Parameters needed \"cd <directory_name>\"\n");
+		     return 0;}
+		else if( (strlen(buff)-1) > (strlen(_cd)+1) && buff[2]==' ' )
+	             return 1;
+
+	}
+	if(!strncmp(buff,_cat,strlen(_cat))){
+		if( (strlen(buff)-1)==strlen(_cat) ){
+		     printf("ERROR: Parameters needed \"cat <file_name>\"\n");
+		     return 0;}
+		else if( (strlen(buff)-1) > (strlen(_cat)+1) && buff[3]==' ' )
+	             return 1;
+
+	}
+	if(!strncmp(buff,_ls,strlen(_ls))){
+		if( (strlen(buff)-1)==strlen(_ls) )
+	             return 1;
+
+	}
+	if(!strncmp(buff,_pwd,strlen(_pwd))){
+		if( (strlen(buff)-1)==strlen(_pwd) )
+	             return 1;
+	}
+	if(!strncmp(buff,_exit,strlen(_exit))){
+		if( (strlen(buff)-1)==strlen(_exit) )
+	             return -1;
+	}
+
+printf("ERROR: Wrong command\n");
+return 0;
+}
+
+
